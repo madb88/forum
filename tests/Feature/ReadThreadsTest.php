127638f3e94ecced7e6d17135f36bda0628b9ea4
madb88 <kaminskiqba@gmail.com>
2017-06-04 20:24:32 +0200

    Added new unit test for reply, added reply relation

---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+    
+    public function setUp()
+    {
+        parent::setUp();
+        $this->thread = factory('App\Thread')->create();
+
+    }
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_a_user_can_view_all_threads()
+    {
+        
+        $response = $this->get('/threads')
+                ->assertSee($this->thread->title);
+        
+       
+    }
+    
+    public function test_a_user_can_view_single_test(){
+
+        $response = $this->get('/threads/' . $this->thread->id)
+                ->assertSee($this->thread->title);
+    }
+    
+    public function test_a_user_can_read_replies_that_are_associated_with_a_thread(){
+        $reply = factory('App\Reply')
+                ->create(['thread_id' => $this->thread->id]);
+        
+        $response = $this->get('/threads/' . $this->thread->id)
+                ->assertSee($reply->body);
+    }
+    
+    
+}
---
+++ tests/Unit/ReplyTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ReplyTest extends TestCase
+{
+    use DatabaseMigrations;
+    public function test_it_has_an_owner(){
+        $reply = factory('App\Reply')->create();
+        $this->assertInstanceOf('App\User', $reply->owner);
+    }
+}
