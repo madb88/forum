256503b2cfe91452636d9912cdc84e1e682942b2
madb88 <kaminskiqba@gmail.com>
2017-06-12 21:13:22 +0200

    New tests for channel, able to filter by channel, new relation for threads and channels.

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Thread;
+use App\Channel;
 use Illuminate\Http\Request;
 
 class ThreadsController extends Controller
@@ -23,9 +24,17 @@ class ThreadsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Channel $channel)
     {
-        $threads = Thread::latest()->get();
+
+        if($channel->exists){
+            //get threads connected with given channel
+            $threads = $channel->threads()->latest()->get();
+        } else {
+            //get all the threads
+            $threads = Thread::latest()->get();
+        }
+
         return view('threads.index', compact('threads'));
 
     }
--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -49,5 +49,16 @@ class ReadThreadsTest extends TestCase
     }
 
 
+    public function test_a_user_can_filter_threads_according_to_a_channel()
+    {
+        $channel = create('App\Channel');
+
+        $threadInChannel = create('App\Thread', ['channel_id' => $channel->id]);
+        $threadNotInChannel = create('App\Thread');
+
+        $this->get('/threads/' . $channel->slug)
+        ->assertSee($threadInChannel->title)
+        ->assertDontSee($threadNotInChannel->title);
+    }
     
 }
